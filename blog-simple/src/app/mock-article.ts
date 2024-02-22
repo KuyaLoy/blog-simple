e6010a3e00fa71cc2e6d65f7e93a3279eea450aa
Robin Tapiru <robintapiru0894@gmail.com>
2024-02-22 16:40:02 +0400

    hmm

---
+++ blog-simple/src/app/mock-article.ts
@@ -0,0 +1,25 @@
+import { Article } from "./article";
+
+export const ARTICLES: Article[] = [
+  {
+    id: 1,
+    title: "My First article",
+    content:
+      "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>",
+    description: "This is my first article! It's great. Please read it. :)",
+    key: "my-first-article",
+    date: new Date(),
+    imageUrl: "https://angular.io/assets/images/logos/angular/angular.svg",
+  },
+  {
+    id: 2,
+    title: "My Second article",
+    content:
+      "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>",
+    description: "Also a greate article!",
+    key: "my-second-article",
+    date: new Date(),
+    imageUrl:
+      "https://angular.io/assets/images/logos/angular/angular_solidBlack.svg",
+  },
+];
--- src/app/article.ts
+++
@@ -1,9 +0,0 @@
-export class Article {
-  id: number;
-  title: string = "";
-  key: string = "";
-  date: Date = new Date();
-  content: string = "";
-  description: string = "";
-  imageUrl: string = "";
-}
